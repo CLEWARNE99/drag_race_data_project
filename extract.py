6d81c789acae0f23ce321c680e22181028a2fb29
CLEWARNE99 <153346467+CLEWARNE99@users.noreply.github.com>
2026-07-09 14:01:01 -0500

    Add files via upload
    Added contestant progress data scrape/csv export.

--- extract.py
+++ extract.py
@@ -61,7 +61,7 @@ def scrape_contestants(season_number):
         contestant_names.append(contestant)
 
     #Get rows to list.
-    for tr in contestants_table.find_all("tr"):
+    for tr in contestants_table.select("tr:not([class='mw-empty-elt'])"):
         cr_cells = []
         for td in tr.find_all("td"):
             cr_cells.append(td.get_text(strip=True))
@@ -82,23 +82,164 @@ def scrape_contestants(season_number):
 
     return contestant_cols, contestant_rows
 
-#Initialize list to hold data that will be used to write csv.
-season_data = []
 
-#Scrape data for each season.
-for season in range (1,19):
-    season_data.append(scrape_contestants(season)[1])
+def scrape_contestants_all_seasons():
+    """
+    Returns scraped contestant data for each season.
+    """
+    # Initialize list to hold data that will be used to write csv.
+    contestant_data = []
 
-    #Sleep as courtesy.
-    time.sleep(1)
+    #Scrape data for each season.
+    for season in range (1,19):
+        contestant_data.append(scrape_contestants(season)[1])
 
-#Create or overwrite csv file with scraped data.
-with open("rpdr_contestant_data.csv", "w", newline="", encoding="utf-8") as file:
-    writer = csv.writer(file)
+        #Sleep as courtesy.
+        time.sleep(1)
 
-    #Add header row.
-    writer.writerow(["Season", "Contestant", "Age", "Hometown", "Placement"])
+    return contestant_data
 
-    for season in season_data:
-        for data_row in season:
-            writer.writerow(data_row)
+def create_contestants_csv():
+    """
+    Creates csv file with scraped contestant data.
+    """
+
+    contestant_data = scrape_contestants_all_seasons()
+
+    with open("rpdr_contestant_data.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        #Add header row.
+        writer.writerow(["Season", "Contestant", "Age", "Hometown", "Placement"])
+
+        for season in contestant_data:
+            for data_row in season:
+                writer.writerow(data_row)
+
+def scrape_progress(season_number):
+    """
+    Takes season number, and scrapes progress data from that season.
+    """
+
+    # Create user_agent per Wikipedia bot policy.
+    user_agent = {"User-Agent": "Drag_Race_Scraping_Test/1.0 (github.com/CLEWARNE99)"}
+
+    # url to use for scraping with changeable season_number part of url for iteration.
+    url = f"https://en.wikipedia.org/wiki/RuPaul's_Drag_Race_season_{season_number}"
+
+    # html text from the request.
+    html_text = requests.get(url, headers=user_agent).text
+
+    soup = BeautifulSoup(html_text, "lxml")
+
+    # Find all tables on page.
+    tables = soup.find_all("table")
+
+    # Initialize contestants table, and progress table.
+    progress_table = None
+
+    # Initialize caption list to use for filtering through tables.
+    caption_list = []
+
+    for table in tables:
+        caption = table.find("caption")
+        try:
+            caption_list.append(caption.text)
+        except AttributeError:
+            continue
+
+        #Identify progress table via caption.
+        try:
+            if "progress" in caption.text or "Progress" in caption.text:
+                progress_table = table
+        except AttributeError:
+            continue
+
+        if progress_table is not None:
+            break
+
+    progress_cols = []
+    progress_rows = []
+
+    contestant_names = []
+
+    if season_number != 10:
+        for th in progress_table.select("th:first-child:not([scope='col'])"):
+            contestant = th.get_text(strip=True)
+            contestant_names.append(contestant)
+    else:
+        for th in progress_table.select("th:first-child"):
+            contestant = th.get_text(strip=True)
+            contestant_names.append(contestant)
+
+    for tr in progress_table.find_all("tr"):
+        cr_cells = []
+        for td in tr.find_all("td"):
+            if td.get_text(strip=True):
+                cr_cells.append(td.get_text(strip=True))
+            else:
+                try:
+                    for num in range(int(td.get("colspan"))):
+                        cr_cells.append("")
+                except TypeError:
+                    cr_cells.append("")
+                except ValueError:
+                    for num in range(int(td.get("colspan")[0:2])):
+                        cr_cells.append("")
+        progress_rows.append(cr_cells)
+
+        """
+        cr_cells = []
+        for td in tr.find_all("td"):
+            cr_cells.append(td.get_text(strip=True))
+        progress_rows.append(cr_cells)
+        """
+
+    progress_rows.pop(0)
+    progress_rows.pop(0)
+
+    index = 0
+    for row in progress_rows:
+        row.insert(0, contestant_names[index])
+        index += 1
+
+    return progress_rows
+
+def scrape_progress_all_seasons():
+    """
+    Returns scraped progress data from each season.
+    """
+    # Initialize list to hold data that will be used to write csv.
+    progress_data = []
+
+    # Scrape data for each season.
+    for season in range(1, 19):
+        progress_data.append(scrape_progress(season))
+
+        # Sleep as courtesy.
+        time.sleep(1)
+
+    return progress_data
+
+def create_progress_csv():
+    """
+    Creates csv file with scraped progress data from each season.
+    """
+
+    progress_data = scrape_progress_all_seasons()
+    header_row = []
+    header_row.append("Contestant")
+    for num in range(1, 17):
+        header_row.append(f"{num}")
+    with open("rpdr_progress_data.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+
+        #Add header row.
+        writer.writerow(header_row)
+
+        for season in progress_data:
+            for data_row in season:
+                writer.writerow(data_row)
+
+create_contestants_csv()
+create_progress_csv()
