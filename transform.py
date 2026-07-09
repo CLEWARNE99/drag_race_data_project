9b0ec0bd1d860f81d107d93dbcb6a9e58229f00c
CLEWARNE99 <153346467+CLEWARNE99@users.noreply.github.com>
2026-07-09 18:15:53 -0500

    Add files via upload
    Started transform file. Cleaned data from both tables generated from extract.py

---
+++ transform.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+def transform_contestant_data():
+
+    df = pd.read_csv("rpdr_contestant_data.csv")
+
+    #Clean Placement column
+    df["Placement"] = df["Placement"].ffill()
+
+    #Remove wikipedia tags
+    tag_list = ["[a]","[b]","[c]","[d]","[e]","[f]","[g]", "[h]"]
+    for tag in tag_list:
+        df["Contestant"] = df["Contestant"].str.replace(pat=tag, repl="")
+        df["Hometown"] = df["Hometown"].str.replace(pat=tag, repl="")
+        df["Placement"] = df["Placement"].str.replace(pat=tag, repl="")
+
+    #Contestant "Shangela Laquifa Wadley" is listed as just "Shangela" Season 3. Updating
+    #Season 2 name for data consistency.
+    df["Contestant"] = df["Contestant"].replace({"Shangela Laquifa Wadley": "Shangela"})
+
+    #Make runner-up string consistent
+    df["Placement"] = df["Placement"].replace({"Runners-up": "Runner-up"})
+
+    #Translate placement colum vals to numerical values, 0 represents "Disqualified"
+    df["Placement"] = df["Placement"].replace({"Winner": "1"})
+    df["Placement"] = df["Placement"].replace({"Runner-up": "2"})
+    df["Placement"] = df["Placement"].replace({"Disqualified": "0"})
+    df["Placement"] = df["Placement"].str.replace("[A-Za-z]", repl="", regex=True)
+
+    #Strip whitespace
+    df["Placement"] = df["Placement"].str.strip()
+
+    #Transform data types
+    #Transform from string into numerical value for age and placement cols
+    df["Season"] = df["Season"].astype(int)
+    df["Age"] = df["Age"].astype(int)
+    df["Placement"] = df["Placement"].astype(int)
+
+    print(df.to_string())
+
+def transform_progress_data():
+    df = pd.read_csv("rpdr_progress_data.csv")
+
+
+    #Remove wikipedia tags
+    tag_list = ["[a]", "[b]", "[c]", "[d]", "[e]", "[f]", "[g]", "[h]"]
+    for tag in tag_list:
+        df["Contestant"] = df["Contestant"].str.replace(pat=tag, repl="")
+        for num in range(1, 18):
+            df[f"{num}"] = df[f"{num}"].str.replace(pat=tag, repl="")
+
+    #Swap columns 1 and 2
+    col_2 = df.pop("Contestant")
+    df.insert(0, "Contestant", col_2)
+
+
+    print(df.to_string())
+transform_contestant_data()
+transform_progress_data()
